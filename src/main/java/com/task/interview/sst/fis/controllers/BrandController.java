516d78079a8d977c8f5431ac6d272fa1f2860e8f
Radzio <radekdrabina@o2.pl>
2020-08-26 20:42:54 +0200

    cross origin enabled for controllers

--- src/main/java/com/task/interview/sst/fis/controllers/BrandController.java
+++ src/main/java/com/task/interview/sst/fis/controllers/BrandController.java
@@ -5,16 +5,14 @@ import com.task.interview.sst.fis.services.BrandService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 import java.util.Set;
 
 @RestController
 @RequestMapping("/api/v1/brands")
+@CrossOrigin("*")
 public class BrandController {
 
     private final BrandService brandService;
--- src/main/java/com/task/interview/sst/fis/controllers/CarPartController.java
+++ src/main/java/com/task/interview/sst/fis/controllers/CarPartController.java
@@ -14,6 +14,7 @@ import java.util.Set;
 
 @RestController
 @RequestMapping("/api/v1/car-parts")
+@CrossOrigin("*")
 public class CarPartController {
 
     private final CarPartService carPartService;
