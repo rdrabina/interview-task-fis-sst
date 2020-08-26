b381cbefa926586ff6ac4bf3ec14b194a7d7b251
Radzio <radekdrabina@o2.pl>
2020-08-26 21:19:50 +0200

    Revert "swagger finally correctly configured"
    This reverts commit 18fb86a4

--- src/main/java/com/task/interview/sst/fis/configs/SwaggerConfiguration.java
+++ src/main/java/com/task/interview/sst/fis/configs/SwaggerConfiguration.java
@@ -2,6 +2,9 @@ package com.task.interview.sst.fis.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -12,7 +15,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfiguration {
+@Profile("!test")
+public class SwaggerConfiguration extends WebMvcConfigurationSupport {
 
     @Bean
     public Docket productApi() {
@@ -21,6 +25,15 @@ public class SwaggerConfiguration {
                 .apis(RequestHandlerSelectors.basePackage("com.task.interview.sst.fis.controllers"))
                 .build()
                 .apiInfo(apiInfo());
+    }
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
     private ApiInfo apiInfo() {
