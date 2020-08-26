9a870bf83b70832827fb744ad0c7757b626167f7
Radzio <radekdrabina@o2.pl>
2020-08-26 20:25:37 +0200

    cross origin disabled for brand controller

--- src/main/java/com/task/interview/sst/fis/controllers/BrandController.java
+++ src/main/java/com/task/interview/sst/fis/controllers/BrandController.java
@@ -5,14 +5,16 @@ import com.task.interview.sst.fis.services.BrandService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 import java.util.Set;
 
 @RestController
 @RequestMapping("/api/v1/brands")
-@CrossOrigin("*")
 public class BrandController {
 
     private final BrandService brandService;
